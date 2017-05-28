e045c626a9302604935ae6c935f4515c254c0a81
Michael Angelo <mikeangelo123@gmail.com>
2017-05-27 21:50:22 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,7 +24,17 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-
+    Todo.find().then((todos) => {
+        res.send({
+            // Sending back the array as an object member makes it
+            // more flexible to add other properties later.
+            todos: todos
+        })
+    }, (err) => {
+        res
+        .status(400)
+        .send(err);
+    });
 });
 
 app.listen(3000, () => {
