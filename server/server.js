ddabe80cee4e7c06cf88ac78d433fd65cc731d50
Michael Angelo <mikeangelo123@gmail.com>
2017-05-28 18:48:07 -0400

    Add PATH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -76,6 +77,39 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((err) => {
         res.status(400).send();
     });
+});
+
+// Update a Todo
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    // Use Lodash to only pull off the properties that we want.
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }
+    else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send(todo);
+    })
+    .catch((err) => {
+        res.status(400).send();
+    });
+
+
 });
 
 app.listen(port, () => {
