eff3802312664887bdb5e9cf26822926c2b9b19c
Michael Angelo <mikeangelo123@gmail.com>
2017-05-28 00:15:36 -0400

    Prepare for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,14 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+// MONGODB_URI is set on Heroku
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://mangelo:malip7557@ds155811.mlab.com:55811/mangelo-todoapp'
+};
+
+mongoose.connect(process.env.PORT ? db.mlab : db.localhost );
 
 module.exports = {
     mongoose: mongoose
