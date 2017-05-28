623320422431fd354d943eb0ee6805d4e23eeffe
Michael Angelo <mikeangelo123@gmail.com>
2017-05-28 19:37:50 -0400

    Set up separate test and development envs

---
+++ server/config/config.js
@@ -0,0 +1,14 @@
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+}
+else if (env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+else {
+    // production
+    process.env.MONGODB_URI = 'mongodb://mangelo:malip7557@ds155811.mlab.com:55811/mangelo-todoapp';
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,12 +3,12 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // MONGODB_URI is set on Heroku
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://mangelo:malip7557@ds155811.mlab.com:55811/mangelo-todoapp'
-};
+// let db = {
+//     localhost: 'mongodb://localhost:27017/TodoApp',
+//     mlab: 'mongodb://mangelo:malip7557@ds155811.mlab.com:55811/mangelo-todoapp'
+// };
 
-mongoose.connect(process.env.PORT ? db.mlab : db.localhost );
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
     mongoose: mongoose
